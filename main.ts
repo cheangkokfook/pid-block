2ffc123987f4d8a333828995cbc24261c176aad9
cheangkokfook <141533007+cheangkokfook@users.noreply.github.com>
2023-08-06 10:57:01 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+let Kp = 0
+let Ki = 0
+let Kd = 0
+let integral = 0
+let previousError = 0
+let error = 0
+let derivative = 0
+let output = 0
+// % block="setPIDConstants $proportional $integralConstant $derivative"
+function setPIDConstants (proportional: number, integralConstant: number, derivative: number) {
+    Kp = proportional
+    Ki = integralConstant
+    Kd = derivative
+}
+// % block="resetPID"
+function resetPID () {
+    integral = 0
+    previousError = 0
+}
+// % block="computePID $setPoint $measureValue"
+function computePID (setPoint: number, measuredValue: number) {
+    error = setPoint - measuredValue
+    integral += error
+    derivative = error - previousError
+    output = Kp * error + Ki * integral + Kd * derivative
+    previousError = error
+}
+basic.forever(function () {
+	
+})
